17ec71f3f831ec90e3f2a220b4d0e3e9f012a3f8
Caroline Pospelova <c.pospelova@smartweb.com.ua>
2024-07-28 08:09:57 +0300

    add seach_vector to Puzzle, add all_puzzles catalogquery with search, filtering and enum

--- puzzles/settings.py
+++ puzzles/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django.contrib.postgres",
-    'graphene_django',
     'puzzles.catalog',
     'puzzles.account',
     'puzzles.rental',
